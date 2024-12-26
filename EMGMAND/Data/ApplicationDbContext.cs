e22a94da8f892ba7a14da9fb700f20deeab835cf
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-12-26 09:04:00 +0000

    Application des validations au niveau de la base e données

--- EMGMAND/Data/ApplicationDbContext.cs
+++ EMGMAND/Data/ApplicationDbContext.cs
@@ -4,10 +4,39 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EMGMAND.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuration de l'entité Car
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.Brand)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Model)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Year)
+                    .IsRequired();
+
+                entity.Property(e => e.IsSold)
+                    .IsRequired();
+
+                entity.Property(e => e.IsAvailable)
+                    .IsRequired();
+            });
+        }
     }
 }
