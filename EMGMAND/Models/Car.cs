fc9629554909665dd2173b90903fa5f0680c2f9a
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-12-26 09:01:21 +0000

    Ajout des validations

--- EMGMAND/Models/Car.cs
+++ EMGMAND/Models/Car.cs
@@ -1,22 +1,32 @@
 ﻿namespace EMGMAND.Models;
+
 using System.ComponentModel.DataAnnotations;
 
 public class Car
 {
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "La marque est obligatoire.")]
+    [StringLength(50, ErrorMessage = "La marque ne peut pas dépasser 50 caractères.")]
     public string Brand { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Le modèle est obligatoire.")]
+    [StringLength(50, ErrorMessage = "Le modèle ne peut pas dépasser 50 caractères.")]
     public string Model { get; set; }
 
-    [Required]
-    [Range(2010, 9999)]
+    [Required(ErrorMessage = "L'année est obligatoire.")]
+    [Range(2010, 9999, ErrorMessage = "L'année doit être comprise entre 2010 et l'année actuelle.")]
     public int Year { get; set; }
 
+    [StringLength(500, ErrorMessage = "La description ne peut pas dépasser 500 caractères.")]
     public string Description { get; set; }
-    public string PhotoPath { get; set; }
-    public bool IsSold { get; set; }
-}
 
+    [Url(ErrorMessage = "Veuillez fournir un chemin d'image valide.")]
+    public string PhotoPath { get; set; }
+
+    [Required]
+    public bool IsSold { get; set; } // Indique si la voiture est vendue.
+
+    [Required]
+    public bool IsAvailable { get; set; } // Indique si la voiture est disponible à la vente.
+}
